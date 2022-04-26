4ee0918bc376902b0c52c73aadf54c8216de60f7
JJ Escandor <jj.p.escandor@gmail.com>
2022-04-26 17:08:44 -0400

    updated styling

--- src/ReadMore.js
+++ src/ReadMore.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Modal, Button } from 'react-bootstrap';
+import './About.css';
 
 class ReadMore extends React.Component {
   render() {
     return (
       <>
         <Modal className="mintingNftModal" show={this.props.showModal}>
-          <Modal.Header>
-            <Modal.Title>Profile Bio</Modal.Title>
+          <Modal.Header id="readmoreHeader">
+            <Modal.Title>{`${this.props.devData.name}'s`} Bio</Modal.Title>
           </Modal.Header>
-          <Modal.Body>{this.props.devData}</Modal.Body>
-          <Modal.Footer>
+          <Modal.Body id="readmoreBody">{this.props.devData.bio}</Modal.Body>
+          <Modal.Footer id="readmoreFooter">
             <Button
               onClick={() => {
                 this.props.hideBio();
