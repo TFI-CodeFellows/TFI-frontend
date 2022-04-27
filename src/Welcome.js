1c83488b28c3d3897bb68cad0a71ec7ce6e76596
JJ Escandor <jj.p.escandor@gmail.com>
2022-04-27 14:33:27 -0400

    updated cart functionality

--- src/Welcome.js
+++ src/Welcome.js
@@ -31,20 +31,23 @@ class Welcome extends React.Component {
                 {!this.props.auth0.isAuthenticated &&
                     <div className="welcomeDiv">
                         <div>
-                            <h1 id="welcomeH1">Welcome to Mercado</h1>
+                            <div class='name-animation'>
+                                <h1 class="typewriter">  Welcome to M3rcado</h1>
+                            </div>
                             <LoginButton />
                         </div>
+
                         <div className="explanation">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-                            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                            aliquip ex ea commodo consequat. Duis aute irure dolor in
-                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                            culpa qui officia deserunt mollit anim id est laborum.
+                            <h5>M3rcado is a web 3.0 inspired application that simulates a decentralized application running on blockchain technology.<br />
+
+                                <br />It also simulates the use of ethereum gas to facilitate e-commerce transactions of Non-Fungible Tokens (NFTs).<br />
+
+                                <br />NFTs are unique cryptographic tokens that exist on a blockchain and cannot be replicated.<br />
+
+                                <br />NFTs represent real-world items like artwork and real estate.<br />
+                            </h5>
                         </div>
-                    </div>
-                }
+                    </div>}
             </div>
         )
     }
